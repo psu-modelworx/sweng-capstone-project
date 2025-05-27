7797de789d6541c8c0c32e44c67a7d5e1a1cb22a
lwcrosier <lwc5402@psu.edu>
2025-05-27 01:00:34 +0000

    Added inital tests for accounts/login page

--- proj/automodeler/tests.py
+++ proj/automodeler/tests.py
@@ -1,8 +1,24 @@
 #from django.test import TestCase
 import pytest
-
+from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your tests here.
 def test_func():
     assert 0 == 0
 
+@pytest.mark.django_db
+def test_login_form(client):
+    # Create test user
+    user = User.objects.create_user(username='testuser', password='testpassword')
+    assert not user.is_superuser
+
+    # Test GET on login page
+    url = reverse('login')
+    response = client.get(url)
+    assert response.status_code == 200
+
+    # response = client.post(url, {'username': 'testuser', 'password': 'testpassword'})
+    #
+    #assert response.status_code == 302  # Redirect status code
+    #assert response.url == reverse('') # Need to specify an expected login page 
