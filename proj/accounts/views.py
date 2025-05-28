8f6a283118b0d4fe77fff357ad99eb5e53f04612
lwcrosier <lwc5402@psu.edu>
2025-05-28 02:17:18 +0000

    added error messages to user signup form

--- proj/accounts/views.py
+++ proj/accounts/views.py
@@ -19,22 +19,16 @@ def signup(request):
         user_form = UserCreationForm(request.POST)
         username = user_form['username']
         if user_form.is_valid():
-            #if not User.objects.filter(username=username).exists:
-                #print("Valid form!")
             user_form.save()
             return render(request, "registration/login.html", {})
-            #else:
-            #    print("User exists!")
-            #    return render(request, "registration/signup.html", {}) # Return errors here and display as error text
         else:
-            #print(user_form)
             errors_dict = {}
             for field, errors in user_form.errors.items():
                 errs = []
                 for error in errors:
                     errs.append(error)
-                    print(f"Error in field' {field}': {error}")
+                    print(f"Error in field '{field}': {error}")
                 errors_dict[field] = errs
-            return render(request, "registration/signup.html", {'form_errors': errors_dict})
+            return render(request, "registration/signup.html", { 'form_errors': errors_dict})
     else:
         return HttpResponseNotAllowed()
