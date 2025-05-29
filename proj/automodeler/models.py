df922b64ca6e9519783826b83686ebea621dffda
lwcrosier <lwc5402@psu.edu>
2025-05-29 16:56:14 +0000

    Added initial Dataset class to automodeler/models.py

--- proj/automodeler/models.py
+++ proj/automodeler/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Dataset(models.Model):
+    name = models.CharField(max_length=50)
+    input_fields = models.JSONField()
+    output_fields = models.JSONField()
+    csv_file = models.FileField(upload_to='dataset_uploads/')
+
