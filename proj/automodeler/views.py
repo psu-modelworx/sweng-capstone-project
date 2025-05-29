65a1a3413f938ee5c0bfac55da1d3adda606b425
lwcrosier <lwc5402@psu.edu>
2025-05-29 16:21:09 +0000

    If user is not logged in, redirect to login page

--- proj/automodeler/views.py
+++ proj/automodeler/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import redirect
 
 from .models import *
 
 # Create your views here.
 
 def index(request):
-    return render(request, "automodeler/index.html", {})
+    if request.user.is_authenticated:
+        return render(request, "/automodeler/index.html", {})
+    else:
+        url = reverse("login")
+        return HttpResponseRedirect(url)
