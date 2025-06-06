5d7f16b4756f5360aaf4d2782ebb9fea5bf67713
jpa5886 <jpa5886@psu.edu>
2025-06-06 16:32:32 -0400

    updating wsgi

--- proj/modelworx/wsgi.py
+++ proj/modelworx/wsgi.py
@@ -6,25 +6,26 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
-
 import os
 
-from decouple import AutoConfig
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modelworx.settings')
+from decouple import Config, RepositoryEnv
 
-application = get_wsgi_application()
-
-
-# Try to load .env.production if it exists, otherwise .env
-base_dir = os.path.dirname(os.path.dirname(__file__))
+# Set path to .env.production
+base_dir = os.path.dirname(os.path.abspath(__file__))
 prod_env = os.path.join(base_dir, ".env.production")
 
+# Load .env.production explicitly
 if os.path.exists(prod_env):
-    config = AutoConfig(search_path=os.path.dirname(prod_env))
+    config = Config(RepositoryEnv(prod_env))
 else:
-    config = AutoConfig(search_path=base_dir)
+    config = Config(RepositoryEnv(os.path.join(base_dir, ".env")))  # fallback to .env
 
-os.environ.setdefault("DJANGO_DEBUG", config("DJANGO_DEBUG", default="True"))
-os.environ.setdefault("DJANGO_ALLOWED_HOSTS", config("DJANGO_ALLOWED_HOSTS", default="localhost"))
+# Set environment variables BEFORE Django loads
+os.environ["DJANGO_DEBUG"] = config("DJANGO_DEBUG", default="False")
+os.environ["DJANGO_ALLOWED_HOSTS"] = config("DJANGO_ALLOWED_HOSTS", default="localhost")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "modelworx.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
