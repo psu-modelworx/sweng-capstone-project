671620b8d5f04711a728ec0d9392a07cba6c7c22
Jamie Dunbar <jpd6363@psu.edu>
2025-06-08 15:36:14 -0400

    Including the rest framework authorization token. Updated the fields added to the REST API.

--- proj/automodeler/serializer.py
+++ proj/automodeler/serializer.py
@@ -2,16 +2,16 @@
 # The view set will be used in the modelworx\urls.py file to show user data in an API response.
 from django.contrib.auth.models import User
 from rest_framework import serializers, viewsets
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
 from .permissions import DetermineIfStaffPermissions
 
 class ModelWorxSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User # Defining a model using the Django User model.
-        fields = ['date_joined', 'last_login', 'is_active', 'is_staff', 'username', 'url',] # Chose important fields to be collected and displayed at a modelworx path.
+        fields = ['date_joined', 'last_login', 'is_active', 'is_staff', 'username'] # Chose important fields to be collected and displayed at a modelworx path.
 
 class ModelWorxViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all() # Creating a list of users that will be serialized by the ModelWorkxSerializer class to get chosen fields.
     serializer_class = ModelWorxSerializer # Setting the serializer to the ModelWorxSerializer class.
-    authentication_classes = [SessionAuthentication, BasicAuthentication] # Ensuring someone is signed to show them the complete API response.
+    authentication_classes = [SessionAuthentication, BasicAuthentication, TokenAuthentication] # Ensuring someone is signed to show them the complete API response.
     permission_classes = [DetermineIfStaffPermissions] # Checking that the user has "is_staff" set to true before showing them the API response.
--- proj/modelworx/settings.py
+++ proj/modelworx/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'accounts', # Custom accounts project to create custom views; Works alongside the auth middleware
     'modelworx',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -87,6 +88,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
