70ee3f7052df5be04b37cb1bd727dca4943127a5
lwcrosier <lwc5402@psu.edu>
2025-06-27 16:47:02 -0400

    Converted signup page to use class-based views, extended from the built-in UserCreationForm

--- proj/accounts/urls.py
+++ proj/accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from .views import SignUpView
 
 urlpatterns = [
-    path("signup/", views.signup, name="signup"),
+    #path("signup/", views.signup, name="signup"),
+    path("signup/", SignUpView.as_view(), name="signup")
 ]
