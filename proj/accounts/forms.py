6426804987c5a06e38e7673d302c1337a61e8272
lwcrosier <lwc5402@psu.edu>
2025-07-06 17:01:46 -0400

    Converted login page to class based view.  This now provides a login error message

--- proj/accounts/forms.py
+++ proj/accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class UserRegisterForm(UserCreationForm):
     username = forms.CharField(label="Username", widget=forms.TextInput(
@@ -10,7 +10,16 @@ class UserRegisterForm(UserCreationForm):
     )
     password1 = forms.CharField(label="Password", widget=forms.PasswordInput(
         attrs={'class' : 'form-control'})
-        )
+    )
     password2 = forms.CharField(label="Confirm Password", widget=forms.PasswordInput(
+        attrs={'class' : 'form-control'})
+    )
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label="Username", widget=forms.TextInput(
+        attrs={'class': 'form-control'})
+    )
+    password = forms.CharField(label="Password", widget=forms.PasswordInput(
         attrs={'class' : 'form-control'})
     )
--- proj/accounts/urls.py
+++ proj/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from .views import SignUpView
+from .views import LoginView
 
 urlpatterns = [
     #path("signup/", views.signup, name="signup"),
-    path("signup/", SignUpView.as_view(), name="signup")
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
