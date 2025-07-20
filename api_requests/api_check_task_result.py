061e6157eb48395984e9bf1bf9c0b460a99f3189
Jamie Dunbar <jpd6363@psu.edu>
2025-07-20 11:31:46 -0400

    Adjusted the API request to check task results and remove unnecessary data.

--- api_requests/api_check_task_result.py
+++ api_requests/api_check_task_result.py
@@ -1,4 +1,3 @@
-import json
 import requests
 
 def api_check_task_result():
@@ -10,17 +9,11 @@ def api_check_task_result():
     url = "http://localhost:8000/automodeler/check_task/520e7549-e1ae-4854-9244-4d29c5c71d50/"
     token = "f0c991d35bdcadea6ba41a3131fb8e3da77b292f"
 
-    # The task ID is collected in the API request to run the model.
-    task_id = "520e7549-e1ae-4854-9244-4d29c5c71d50"
-
     # Added the authentication token to the header to authenticate the user.
     headers = { "Authorization": "Token " + token}
 
-    # Added the task ID to the data so it can be sent and accessed.
-    data = {"task_id": task_id}
-
     # Getting the response after making the API request for task results and printing them.
-    response = requests.post(url=url, headers=headers, data=data)
+    response = requests.post(url=url, headers=headers)
     print(response.text)
 
 # The main method which is used to run the function for the API request.
