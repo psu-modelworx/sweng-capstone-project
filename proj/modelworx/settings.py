c687dc5998521e64d2d80f25ec92588aad02706c
Jamie Dunbar <jpd6363@psu.edu>
2025-07-27 12:32:07 -0400

    Added a throttle to limit the number of API requests a user can make in a minute.

--- proj/modelworx/settings.py
+++ proj/modelworx/settings.py
@@ -98,7 +98,15 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '15/min',
+        'user': '15/min'
+    }
 }
 
 # Database
