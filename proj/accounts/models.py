c372fdd89a73f558f9f7230fee466c462f11788a
lwcrosier <lwc5402@psu.edu>
2025-08-09 14:41:36 -0400

    Emailing upona ccount creation is now a celery task

--- proj/accounts/models.py
+++ proj/accounts/models.py
@@ -1,3 +1,15 @@
-#from django.db import models
+from django.db import models
 
 # Create your models here.
+
+class EmailTask(models.Model):
+    TASK_TYPES = [
+        ('emailing', 'Emailing'),
+    ]
+
+    task_id = models.CharField(max_length=255, unique=True)
+    task_type = models.CharField(max_length=50, choices=TASK_TYPES)
+    status = models.CharField(max_length=50, default='PENDING')
+    result_message = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
